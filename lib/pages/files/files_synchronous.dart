74f970792feb3ba9262a2ec0c7d7cf507fa9ed31
Kevin Kruijthof <kev17062004@gmail.com>
2025-07-11 18:15:47 +0200

    ``` feat(files): Add synchronous file view page
    ```

---
+++ lib/pages/files/files_synchronous.dart
@@ -0,0 +1,64 @@
+import 'package:exdock_backoffice/pages/files/blocks/generate_block.dart';
+import 'package:exdock_backoffice/pages/files/top_bar/top_bar.dart';
+import 'package:exdock_backoffice/utils/map_notifier.dart';
+import 'package:flutter/material.dart';
+
+class FilesSynchronous extends StatefulWidget {
+  const FilesSynchronous({
+    super.key,
+    required this.blocks,
+    required this.changeAttributeMap,
+  });
+
+  final Map<String, dynamic> blocks;
+  final MapNotifier changeAttributeMap;
+
+  @override
+  State<FilesSynchronous> createState() => _FilesSynchronousState();
+}
+
+class _FilesSynchronousState extends State<FilesSynchronous> {
+  String path = "";
+
+  @override
+  void initState() {
+    widget.changeAttributeMap.addEntry("path", path);
+    widget.changeAttributeMap.addListener(() {
+      setState(() {
+        path = widget.changeAttributeMap.value["path"] ?? "";
+      });
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    widget.changeAttributeMap.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        TopBar(path: path),
+        Padding(
+          padding: const EdgeInsets.only(top: 80),
+          child: ListView(
+            children: List<Widget>.generate(
+              widget.blocks.length,
+              (index) {
+                return GenerateBlock(
+                  block: widget.blocks.entries.toList()[index],
+                  changeAttributeMap: widget.changeAttributeMap,
+                );
+              },
+            ),
+          ),
+        )
+      ].reversed.toList(),
+    );
+  }
+}
