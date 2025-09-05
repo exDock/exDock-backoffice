4a92075556191aee9bc302b69d5a3a453b0c8bb9
Kevin Kruijthof <kev17062004@gmail.com>
2025-09-05 16:15:44 +0200

    ``` feat(router): Add page info route to content routes
    ```

--- lib/router/routes/content.dart
+++ lib/router/routes/content.dart
@@ -1,11 +1,10 @@
 // Flutter imports:
-import 'package:flutter/material.dart';
-
-// Package imports:
-import 'package:go_router/go_router.dart';
-
+import 'package:exdock_backoffice/pages/content/pages/page_info/page_info.dart';
 // Project imports:
 import 'package:exdock_backoffice/pages/content/pages/pages_overview.dart';
+import 'package:flutter/material.dart';
+// Package imports:
+import 'package:go_router/go_router.dart';
 
 List<GoRoute> getContentRoutes() {
   return [
@@ -17,5 +16,16 @@ List<GoRoute> getContentRoutes() {
       path: '/content/pages',
       builder: (context, state) => const PagesOverview(),
     ),
+    GoRoute(
+        path: '/content/pages/:key',
+        builder: (context, state) {
+          String? key;
+          try {
+            key = state.pathParameters['key'];
+          } catch (_) {}
+          return PageInfo(
+            url: key!,
+          );
+        })
   ];
 }
