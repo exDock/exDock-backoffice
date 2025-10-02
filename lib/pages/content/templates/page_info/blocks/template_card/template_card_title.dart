98d784ccf96006f64088d5b917fea7bca61cdc09
Kevin Kruijthof <kev17062004@gmail.com>
2025-10-02 15:22:37 +0200

    feat(page-editor): Add TemplateCardTitle widget
    Adds TemplateCardTitle for page templates:
    
    - Implements a reusable card component
    - Includes title, content, and optional buttons
    - Enhances page template UI
    
    This improves the structure and usability of page template editing.

---
+++ lib/pages/content/templates/page_info/blocks/template_card/template_card_title.dart
@@ -0,0 +1,51 @@
+// Flutter imports:
+// Project imports:
+import 'package:exdock_backoffice/pages/content/templates/page_info/blocks/template_card/template_card.dart';
+import 'package:flutter/material.dart';
+
+class TemplateCardTitle extends StatelessWidget {
+  const TemplateCardTitle({
+    super.key,
+    this.width = double.infinity,
+    required this.title,
+    required this.child,
+    this.button,
+    this.middleButton,
+  });
+
+  final double width;
+  final String title;
+  final Widget child;
+  final Widget? button;
+  final Widget? middleButton;
+
+  @override
+  Widget build(BuildContext context) {
+    return TemplateCard(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              if (middleButton != null) ...[
+                const SizedBox(width: 12),
+                middleButton!,
+              ],
+              if (button != null) ...[
+                const SizedBox(width: 12),
+                button!,
+              ]
+            ],
+          ),
+          const SizedBox(height: 12),
+          child,
+        ],
+      ),
+    );
+  }
+}
