5e8bf14b880616a8189a710e38513740cb4e07dc
Kevin Kruijthof <kev17062004@gmail.com>
2025-10-02 15:04:09 +0200

    fix(content): Update content routes and page info
    Update content routing to use templates and new page info component.
    
    * Replace page overview with templates overview.
    * Add routes for creating and editing templates.
    * Update page info component to support template creation.

--- lib/router/routes/content.dart
+++ lib/router/routes/content.dart
@@ -1,7 +1,8 @@
 // Flutter imports:
-import 'package:exdock_backoffice/pages/content/pages/page_info/page_info.dart';
 // Project imports:
 import 'package:exdock_backoffice/pages/content/pages/pages_overview.dart';
+import 'package:exdock_backoffice/pages/content/templates/page_info/page_info.dart';
+import 'package:exdock_backoffice/pages/content/templates/templates_overview.dart';
 import 'package:flutter/material.dart';
 // Package imports:
 import 'package:go_router/go_router.dart';
@@ -17,15 +18,32 @@ List<GoRoute> getContentRoutes() {
       builder: (context, state) => const PagesOverview(),
     ),
     GoRoute(
-        path: '/content/pages/:key',
-        builder: (context, state) {
-          String? key;
-          try {
-            key = state.pathParameters['key'];
-          } catch (_) {}
-          return PageInfo(
-            url: key!,
-          );
-        })
+      path: "/content/templates",
+      builder: (context, state) {
+        return const TemplatesOverview();
+      },
+    ),
+    GoRoute(
+      path: '/content/templates/newTemplate',
+      builder: (context, state) {
+        return const PageInfo(
+          url: "",
+          isNewPage: true,
+        );
+      },
+    ),
+    GoRoute(
+      path: '/content/templates/:key',
+      builder: (context, state) {
+        String? key;
+        try {
+          key = state.pathParameters['key'];
+        } catch (_) {}
+        return PageInfo(
+          url: key!,
+          isNewPage: false,
+        );
+      },
+    ),
   ];
 }
