b5001444ec4ece40addc481ce14f295e6c394b38
Kevin Kruijthof <kev17062004@gmail.com>
2025-10-02 15:23:07 +0200

    feat(page-editor): Add TopBar widget
    Adds a new TopBar widget for page info templates.
    
    - Implements a top bar with title and save button.
    - Includes an IdList component for ID management.
    - Uses a MapNotifier for save state tracking.

---
+++ lib/pages/content/templates/page_info/blocks/top_bar/top_bar.dart
@@ -0,0 +1,75 @@
+// Flutter imports:
+// Project imports:
+import 'package:exdock_backoffice/globals/globals.dart';
+import 'package:exdock_backoffice/pages/content/templates/page_info/blocks/top_bar/id_list.dart';
+import 'package:exdock_backoffice/utils/map_notifier.dart';
+import 'package:exdock_backoffice/widgets/buttons/exdock_save_button.dart';
+import 'package:flutter/material.dart';
+
+class TopBar extends StatefulWidget {
+  const TopBar({
+    super.key,
+    required this.name,
+    required this.saveNotifier,
+    required this.saveValues,
+  });
+
+  final String name;
+  final MapNotifier saveNotifier;
+  final Function saveValues;
+
+  @override
+  State<TopBar> createState() => _TopBarState();
+}
+
+class _TopBarState extends State<TopBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      decoration: const BoxDecoration(
+        color: darkColour,
+        boxShadow: kBoxShadowList,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(
+            width: 35,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Text(
+              widget.name,
+              style: const TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const Expanded(
+            child: SizedBox(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IdList(
+              saveNotifier: widget.saveNotifier,
+            ),
+          ),
+          const SizedBox(
+            width: 36,
+          ),
+          ExDockSaveButton(
+            somethingToSaveNotifier: widget.saveNotifier,
+            onPressed: () {
+              widget.saveValues();
+            },
+          ),
+          const SizedBox(
+            width: 44,
+          )
+        ],
+      ),
+    );
+  }
+}
