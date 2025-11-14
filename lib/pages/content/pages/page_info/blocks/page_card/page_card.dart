09c9afa17912c9144b1e233a48c554860cbbc29e
Kevin Kruijthof <kev17062004@gmail.com>
2025-11-14 14:52:13 +0100

    feat(page-info): Add PageCard widget
    Add reusable card for page content.
    
    - Create `PageCard` component
    - Style using `exdock_backoffice` theme
    - Center child content with padding

---
+++ lib/pages/content/pages/page_info/blocks/page_card/page_card.dart
@@ -0,0 +1,34 @@
+import 'package:exdock_backoffice/globals/styling.dart';
+import 'package:flutter/material.dart';
+
+class PageCard extends StatelessWidget {
+  const PageCard({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.5 - 96,
+      decoration: BoxDecoration(
+        color: Theme.of(context).cardColor,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: kBoxShadowList,
+      ),
+      clipBehavior: Clip.hardEdge,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(24),
+            child: Center(
+              child: child,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
