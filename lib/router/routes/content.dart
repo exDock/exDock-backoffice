c389e4eedb893fe2cf324979d7d02fb60f3ab074
Kevin Kruijthof <kev17062004@gmail.com>
2025-11-14 14:51:23 +0100

    fix(content): Update content routes and page structure
    This commit refactors the content routes and page structure to improve navigation and organization.
    
    - Refactor content routes using go_router.
    - Update page structure for page and template info.
    - Improve navigation for new pages and templates.

--- lib/router/routes/content.dart
+++ lib/router/routes/content.dart
@@ -1,15 +1,15 @@
 // Flutter imports:
 
+// Project imports:
+import 'package:exdock_backoffice/pages/content/pages/page_info/page_info.dart';
+import 'package:exdock_backoffice/pages/content/pages/page_preview.dart';
+import 'package:exdock_backoffice/pages/content/pages/pages_overview.dart';
+import 'package:exdock_backoffice/pages/content/templates/page_info/template_info.dart';
+import 'package:exdock_backoffice/pages/content/templates/templates_overview.dart';
 // Flutter imports:
 import 'package:flutter/material.dart';
-
 // Package imports:
 import 'package:go_router/go_router.dart';
-
-// Project imports:
-import 'package:exdock_backoffice/pages/content/pages/pages_overview.dart';
-import 'package:exdock_backoffice/pages/content/templates/page_info/page_info.dart';
-import 'package:exdock_backoffice/pages/content/templates/templates_overview.dart';
 
 List<GoRoute> getContentRoutes() {
   return [
@@ -22,6 +22,42 @@ List<GoRoute> getContentRoutes() {
       builder: (context, state) => const PagesOverview(),
     ),
     GoRoute(
+        path: '/content/pages/newPage',
+        builder: (context, state) {
+          return const PageInfo(
+            url: "",
+            isNewPage: true,
+          );
+        }),
+    GoRoute(
+        path: '/content/pages/:key',
+        builder: (context, state) {
+          String? key;
+          try {
+            key = state.pathParameters['key'];
+          } catch (_) {}
+          return PageInfo(
+            url: key!,
+            isNewPage: false,
+          );
+        }),
+    GoRoute(
+      path: '/content/page_preview',
+      builder: (context, state) {
+        final Map<String, dynamic> queryParams = state.uri.queryParameters;
+        final List<String> templateIds = [];
+        if (queryParams.containsKey('templateIds')) {
+          final String templateIdsString =
+              queryParams["templateIds"].toString();
+          templateIds.addAll(templateIdsString.split(';'));
+        }
+
+        return PagePreview(
+          templateIds: templateIds,
+        );
+      },
+    ),
+    GoRoute(
       path: "/content/templates",
       builder: (context, state) {
         return const TemplatesOverview();
@@ -30,7 +66,7 @@ List<GoRoute> getContentRoutes() {
     GoRoute(
       path: '/content/templates/newTemplate',
       builder: (context, state) {
-        return const PageInfo(
+        return const TemplateInfo(
           url: "",
           isNewPage: true,
         );
@@ -43,7 +79,7 @@ List<GoRoute> getContentRoutes() {
         try {
           key = state.pathParameters['key'];
         } catch (_) {}
-        return PageInfo(
+        return TemplateInfo(
           url: key!,
           isNewPage: false,
         );
